411b8d49fd6b5a4d539d654e5397e0b450758220
gschernyshov <grchafv@yandex.ru>
2026-02-21 19:53:08 +0300

    fix(backend): исправление названия переменной окружения, отвечающей за CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,7 @@ import authRoutes from './routes/auth.routes.js'
 
 export const app = express()
 
-// app.use(cors({ origin: process.env.ORIGIN_CORS || 'http://localhost:3003' }))
+// app.use(cors({ origin: process.env.CORS_ORIGINS || 'http://localhost:3003' }))
 app.use(cors())
 app.use(express.json({ limit: '10mb' }))
 
